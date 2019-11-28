02a938d0a86a8bdc9bc7180788e74d19bc8321fd
davidhaddad7 <42075126+davidhaddad7@users.noreply.github.com>
2019-11-27 17:15:26 -0800

    updated main with a while loop for reading lines from csv , also added macros

--- maxTweeter.c
+++ maxTweeter.c
@@ -5,6 +5,9 @@
 #include <fcntl.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 1024
+#define MAX_FILE_LENGTH 20000
+
 typedef struct tweeter {
 	char* tweeter;
 	int count;
@@ -12,21 +15,56 @@ typedef struct tweeter {
 
 int main(int argc, char** argv)
 {
-	if (argc != 2 || argv[0] == NULL || argv[1] == NULL) {
-		perror("invalid input\n");
-		return -1;
-	}
+    /* 
+     * 1. read csv line by line 
+     * 2. error check the max length of each line
+     * 3. error check the max number of lines in file 
+     * 
 
-	FILE* fd = fopen(argv[1], "r");
-	if (fd == NULL) {
-		fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
-		return -1;
-	}
+     
+     * A valid input csv file has columns separated with “,”’s, but you cannot 
+     * assume the location of the tweeter column will be fixed to a particular
+     * location (like 8 for instance).
+     
 
-	if (fclose(fd) != 0) {
-		fprintf(stderr, "file close failure: %s\n", strerror(errno));
-		return -1;
-	}
+     
+     * A valid csv file will have a header, and the tweeter column will be called
+     * “name”, and the items may or may not be surrounded by quotes (“/”).
+     
 
-	return 0;
+    
+    * a valid file will not have additional commas inside the tweet. However, 
+    * your program should not crash on any input, even invalid ones
+    */
+    printf("Starting HW maxTweeter\n");
+    if (argc != 2 || argv[0] == NULL || argv[1] == NULL) {
+      perror("invalid input\n");
+      return -1;
+    }
+
+    FILE* fd = fopen(argv[1], "r");
+    if (fd == NULL) {
+      fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
+      return -1;
+    }
+    printf("Input file: %s\n", argv[1]);
+
+    char line[MAX_LINE_LENGTH];
+    int lines = 0;
+    // float lenTotal = 0;
+
+    while (fgets(line, MAX_LINE_LENGTH, fd))
+      {
+          char* tmp = strdup(line);
+          printf("line %d contains: %s", lines,tmp);
+          lines++;
+
+      }
+
+    if (fclose(fd) != 0) {
+      fprintf(stderr, "file close failure: %s\n", strerror(errno));
+      return -1;
+    }
+
+    return 0;
 }
