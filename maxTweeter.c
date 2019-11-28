f1dd2cc699695f75af96002fd26f5537ee6b1896
davidhaddad7 <42075126+davidhaddad7@users.noreply.github.com>
2019-11-27 22:21:18 -0800

    checked for max lines and max characters (incomplete)
    needs more work to check if line 1 is valid or empty

--- maxTweeter.c
+++ maxTweeter.c
@@ -4,9 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdbool.h>
 
-#define MAX_LINE_LENGTH 1024
-#define MAX_FILE_LENGTH 20000
+#define MAX_CHAR_PER_LINE 1024
+#define MAX_LINES_PER_FILE 20000
 
 typedef struct tweeter {
 	char* tweeter;
@@ -16,50 +18,57 @@ typedef struct tweeter {
 int main(int argc, char** argv)
 {
     /* 
-     * 1. read csv line by line 
-     * 2. error check the max length of each line
-     * 3. error check the max number of lines in file 
-     * 
-
-     
      * A valid input csv file has columns separated with “,”’s, but you cannot 
      * assume the location of the tweeter column will be fixed to a particular
      * location (like 8 for instance).
      
-
-     
      * A valid csv file will have a header, and the tweeter column will be called
      * “name”, and the items may or may not be surrounded by quotes (“/”).
-     
-
     
-    * a valid file will not have additional commas inside the tweet. However, 
-    * your program should not crash on any input, even invalid ones
-    */
-    printf("Starting HW maxTweeter\n");
+     * a valid file will not have additional commas inside the tweet. However, 
+     * your program should not crash on any input, even invalid ones
+     * 
+     */
+
     if (argc != 2 || argv[0] == NULL || argv[1] == NULL) {
       perror("invalid input\n");
       return -1;
     }
+
+
 
     FILE* fd = fopen(argv[1], "r");
     if (fd == NULL) {
       fprintf(stderr, "%s: %s\n", argv[1], strerror(errno));
       return -1;
     }
+
     printf("Input file: %s\n", argv[1]);
+    char line[MAX_CHAR_PER_LINE];
+    int numberOfLines = 0;  
+    // char ch; 
 
-    char line[MAX_LINE_LENGTH];
-    int lines = 0;
-    // float lenTotal = 0;
+    // /* 
+    //  * 1. read csv line by line 
+    //  * 2. error check the max length of each line (max 1024)
+    //  * 3. error check the max number of lines in file (max 20000)
+    //  */ 
 
-    while (fgets(line, MAX_LINE_LENGTH, fd))
+    numberOfLines = 0;
+    while (fgets(line, MAX_CHAR_PER_LINE, fd))
       {
           char* tmp = strdup(line);
-          printf("line %d contains: %s", lines,tmp);
-          lines++;
-
+          numberOfLines++;
+          printf("line %d contains %s", numberOfLines,tmp);
+          if (numberOfLines == 1 && getc(fd) == EOF)
+            numberOfLines = 0;
       }
+      printf("numberofLines: %d\n", numberOfLines);
+
+    if (numberOfLines <= 0 || numberOfLines >= MAX_LINES_PER_FILE) {
+      perror("invalid number of lines in file\n");
+      return -1;
+    }
 
     if (fclose(fd) != 0) {
       fprintf(stderr, "file close failure: %s\n", strerror(errno));
