70a0ef237b04c11f0540a75e9d8290c2d96b74ed
davidhaddad7 <42075126+davidhaddad7@users.noreply.github.com>
2019-11-29 21:38:38 -0800

    I was able to store the count of each tweeter into an array of structs , i was able to print out the top tweeters but it needs some minor changes to work for different cases

--- maxTweeter.c
+++ maxTweeter.c
@@ -15,6 +15,16 @@ typedef struct tweet {
 	int count;
 } tweet;
 
+int findTweeter(struct tweet *tweeterArrayTemp, char *name ){
+	
+	for (int i = 0; i < sizeof(tweeterArrayTemp); i++){
+		// printf("string: %s name: %s ", tweeterArrayTemp[i].tweeter, name);
+		if (!strcmp(tweeterArrayTemp[i].tweeter, name))
+			return 0;
+	}
+	return -1;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -22,10 +32,8 @@ int main(int argc, char** argv)
 	 * A valid input csv file has columns separated with “,”’s, but you cannot
 	 * assume the location of the tweeter column will be fixed to a particular
 	 * location (like 8 for instance).
-
 	 * A valid csv file will have a header, and the tweeter column will be called
 	 * “name”, and the items may or may not be surrounded by quotes (“/”).
-
 	 * a valid file will not have additional commas inside the tweet. However,
 	 * your program should not crash on any input, even invalid ones
 	 *
@@ -48,6 +56,11 @@ int main(int argc, char** argv)
 	/*
 		You may also assume that the maximum line length of any valid csv file will not be longer than 1024 characters, the length of the file will not exceed 20,000 lines.
 	*/
+ 
+ 	tweet tweeterArray[MAX_LINES_PER_FILE];
+	// tweet* tweetArray =(void*) malloc( sizeof(struct tweet));
+	memset(tweeterArray, 0, MAX_LINES_PER_FILE * sizeof(struct tweet)); 
+	
 
 	while (fgets(line, MAX_CHAR_PER_LINE, fd)) {
 		char* token;
@@ -57,18 +70,57 @@ int main(int argc, char** argv)
 			if (num_lines == 0) {
 				if (!strcmp(token, "name") || !strcmp(token, "\"name\""))
 					tweeter_col = num_cols;
-			} else if (num_cols == tweeter_col)
-				printf("tweeter: %s\n", token);
+			} else if (num_cols == tweeter_col){
+				 if (num_lines < 1 ){
+				perror("invalid number of lines in file\n");
+				return -1;
+				}
+				tweeterArray[num_lines].tweeter = token;
+				tweeterArray[num_lines].count = 1;
+          
+      }
 			num_cols++;
 		}
 		num_lines++;
 	}
 
-
 	if (num_lines <= 0 || num_lines >= MAX_LINES_PER_FILE) {
 		perror("invalid number of lines in file\n");
 		return -1;
 	}
+
+//find the count of all the tweeters
+	for (int i = 1; i< num_lines; i++){
+		for(int j = 1; j < num_lines ; j++){
+			if (!strcmp(tweeterArray[j].tweeter , tweeterArray[i].tweeter) && i != j){
+				tweeterArray[i].count++;
+			}		
+		}
+	}
+
+//array that will hold the final tweeters and their counts
+	tweet finalArray[10];
+	for(int j = 0; j < 10; j++){
+		finalArray[j].tweeter = "";
+		finalArray[j].count = 0;
+	}
+
+//iterates through all names and counts to findthe top 10 tweeters
+	for (int i = 1; i < num_lines; i++){
+		for(int j = 0; j < 10; j++){
+			if (strcmp(finalArray[j].tweeter,tweeterArray[i].tweeter) && tweeterArray[i].count >finalArray[j].count && findTweeter(finalArray, tweeterArray[i].tweeter)){
+				finalArray[j].tweeter = tweeterArray[i].tweeter;
+				finalArray[j].count= tweeterArray[i].count;
+			}		
+		}
+	}
+
+//display the top 10 tweeters and their counts
+	for (int i = 0; i < 10; i++){
+		printf("tweeter: %s, count: %d\n", finalArray[i].tweeter,finalArray[i].count);
+	}
+
+
 
 	if (fclose(fd) != 0) {
 		fprintf(stderr, "file close failure: %s\n", strerror(errno));
